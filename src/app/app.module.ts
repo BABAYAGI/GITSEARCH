a4369a6b289fc20bcd40446ecbd1f4a467fec72f
babayagi <darsan_hirani@hotmail.com>
2019-10-01 10:19:38 +0300

    added components and services

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { SearchappComponent } from './searchapp/searchapp.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+// import { GoalFormComponent } from './goal-form/goal-form.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+
+import { CapsPipe } from './caps.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchappComponent,
+    // GoalFormComponent,
+    SearchFormComponent,
+    CapsPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
